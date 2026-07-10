631b78b7a3f57a59b4456658b9bd9796bbc2a3f4
Spiriali <ameliagns2004@gmail.com>
2026-07-10 10:22:29 -0700

    Added Yarnspinner for Dialogue

---
+++ Assets/Unity Scripts/Dialogue/TriggerDialogue.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using Yarn;
+using Yarn.Unity;
+
+public class TriggerDialogue : MonoBehaviour
+{
+    [SerializeField] GameObject dialogueSystem;
+    private DialogueRunner dialogueRunner;
+    private bool inDialogue;
+    private string dialogueNode;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Dialogue"))
+        {
+            if (collision.gameObject.GetComponent<NPCDialogue>() != null) 
+            {
+                dialogueNode = collision.gameObject.GetComponent<NPCDialogue>().dialogueNode;
+            }
+            inDialogue = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Dialogue"))
+        {
+            inDialogue = false;
+        }
+    }
+
+    private void Start()
+    {
+        dialogueRunner = dialogueSystem.GetComponent<DialogueRunner>();
+    }
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.E) && inDialogue && dialogueNode!=null && !dialogueRunner.IsDialogueRunning)
+        {
+            dialogueRunner.StartDialogue(dialogueNode);
+        }
+    }
+}
