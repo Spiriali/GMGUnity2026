6420615a46cc97c47f96d5c7db388272caf58a1f
cerulean0263-sudo <cerulean0263@gmail.com>
2026-07-15 14:54:25 -0700

    worked on stuffy sanity system and inventory system

--- Assets/Unity Scripts/Inventory.cs
+++ Assets/Unity Scripts/Inventory.cs
@@ -5,9 +5,17 @@ public class Inventory : MonoBehaviour
     //f to interact with object
 
     private bool pickUp;
-    public bool stuffy;
+    public bool stuffy = false;
+    public bool throwable = false;
+    public bool key = false;
 
+    public GameObject stuffyImage;
+    public GameObject keyImage;
+    public GameObject throwableImage;
     private GameObject item;
+    private InventoryItem itemScript;
+
+    public HealthSystemAttribute healthSystem;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("inventory"))
@@ -15,6 +23,7 @@ public class Inventory : MonoBehaviour
            
             pickUp = true;
             item = collision.gameObject;
+            itemScript = item.GetComponent<InventoryItem>();
         }
     }
         
@@ -29,15 +38,60 @@ public class Inventory : MonoBehaviour
     
     private void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            
-        }
         if (pickUp && Input.GetKey(KeyCode.F))
         {
-            
-            item.GetComponent<InventoryItem>().Disappear();
+            StuffyPickUp();
+            ThrowablePickUp();
+            KeyPickUp();
+           if (itemScript.disappear) 
+           {
+             itemScript.Disappear();
+           }
+        }
+        if (stuffy && Input.GetKeyDown(KeyCode.Space))
+        {
+            healthSystem.ModifyHealth(1);
         }
     }
     
-}
+    private void StuffyPickUp()
+    {
+        if (itemScript.stuffy) 
+        {
+            stuffy = true;
+            stuffyImage.SetActive(true);
+            healthSystem.stuffy = true;
+        }
+    }
+    public void StuffyLoss()
+    {
+       stuffy = false;
+       stuffyImage.SetActive(false);
+    }
+    private void KeyPickUp()
+    {
+        if (itemScript.key)
+        {
+            key = true;
+            keyImage.SetActive(true);
+        }
+    }
+    public void KeyLoss()
+    {
+        key = false;
+        keyImage.SetActive(false);
+    }
+    private void ThrowablePickUp()
+    {
+        if (itemScript.throwable)
+        {
+            throwable = true;
+            throwableImage.SetActive(true);
+        }
+    }
+    public void ThrowableLoss()
+    {
+        throwable = false;
+        throwableImage.SetActive(false);
+    }
+ }
